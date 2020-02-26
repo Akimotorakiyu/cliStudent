8a340f91a01f1b48fc08cf8d1a592eff355b04f0
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-02-26 16:31:12 +0800

    use webpack-dev-server

--- scripts/webpack.config.ts
+++ scripts/webpack.config.ts
@@ -1,6 +1,9 @@
 import * as path from "path";
 import * as webpack from "webpack";
 
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
 // 分析参数
 const modeIndex = process.argv.findIndex(ele => {
   return ele == "--mode";
@@ -13,9 +16,14 @@ if (modeIndex >= 0) {
   throw "未定义--mode， 默认mode";
 }
 
-
-
-const plugins: webpack.Plugin[] = [];
+const plugins: webpack.Plugin[] = [
+  new CleanWebpackPlugin({
+    cleanOnceBeforeBuildPatterns: ["dist"]
+  }),
+  new HtmlWebpackPlugin({
+    title: "Development"
+  })
+];
 if (isDevelopment) {
 }
 
