64f1398ad5f5d3f767a871d5a638260ecc0b989d
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-03-17 12:27:27 +0800

    use node-dev & show parameters

--- src/commands/cliStudent.ts
+++ src/commands/cliStudent.ts
@@ -1,5 +1,8 @@
+import { GluegunToolbox } from "gluegun";
 export default {
-  run() {
+  async run(toolbox: GluegunToolbox) {
     console.log("defult command");
+    console.log(toolbox.parameters);
+    const spin = toolbox.print.spin("defult command");
   }
 };
